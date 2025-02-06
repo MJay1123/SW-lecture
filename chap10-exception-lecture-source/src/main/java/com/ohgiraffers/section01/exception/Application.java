480cc09e2fb3d0dc0edb67badd1e4ddbdbec21fe
MJay1123 <tjalswhd1@naver.com>
2025-02-06 09:48:28 +0900

    exception챕터 강의파일 추가

--- chap10-exception-lecture-source/src/main/java/com/ohgiraffers/section01/exception/Application.java
+++ chap10-exception-lecture-source/src/main/java/com/ohgiraffers/section01/exception/Application.java
@@ -4,5 +4,23 @@ public class Application {
     public static void main(String[] args) {
 
         ExceptionTest et = new ExceptionTest();
+
+        try {
+//            et.checkEnoughMoney(10000, 50000);
+            et.checkEnoughMoney(50000, 10000);
+            System.out.println("예외가 없었군요. 돈이 많으신가 봐요?");
+        } catch(ArithmeticException e) {
+            System.out.println("예외가 발생했군요!");
+
+            System.out.println("그 예외는 " + e.getMessage() + "때문이군요!!!!");
+            System.out.println("거 돈 좀 넉넉히 들고 다닙시다.");
+//            System.exit(0);                     // 자바 어플리케이션이 즉시 종료되도록 처리
+
+        }
+
+        System.out.println("프로그램을 종료합니다.");
+
+
+
     }
 }
